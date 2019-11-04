8b0f09f0044baca98f4286327cff6c825d17fb55
Solcan Moise <moise.solcan@paddypowerbetfair.com>
2019-11-04 23:38:52 +0200

    Get username and password from admin

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  logindata;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.logindata = new FormGroup({
+         username: new FormControl(""),
+         password: new FormControl("")
+      });
   }
+
+  onClickSubmit(data) {
+      console.log(data.username);
+      if (data.username == "systemadmin" && data.password == "admin123") {
+         alert("Login Successful");
+         this.router.navigate(['/home']);
+      }
+   }
 
 }
